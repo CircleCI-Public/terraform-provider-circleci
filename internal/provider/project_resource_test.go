40c06a9da5eb03b6039ce63a8179ebb8eccd6f82
David Alberto Montaño Fetecua <david.montano@circleci.com>
2025-06-04 15:09:56 -0500

    fix: adding a new test to create and retrieve github projects (#21)

--- internal/provider/project_resource_test.go
+++ internal/provider/project_resource_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
 )
 
-func TestAccProjectResource(t *testing.T) {
+func TestAccProjectResource1(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
@@ -36,6 +36,46 @@ func TestAccProjectResource(t *testing.T) {
 						"circleci_project.test_project",
 						tfjsonpath.New("project_provider"),
 						knownvalue.StringExact("circleci"),
+					),
+					statecheck.ExpectKnownValue(
+						"circleci_project.test_project",
+						tfjsonpath.New("organization_slug"),
+						knownvalue.StringExact("circleci/8e4z1Akd74woxagxnvLT5q"),
+					),
+					statecheck.ExpectKnownValue(
+						"circleci_project.test_project",
+						tfjsonpath.New("organization_id"),
+						knownvalue.StringExact("3ddcf1d1-7f5f-4139-8cef-71ad0921a968"),
+					),
+				},
+			},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
+
+func TestAccProjectResource2(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				Config: testAccProjectResourceConfig(
+					"github_project",
+					"github",
+					"3ddcf1d1-7f5f-4139-8cef-71ad0921a968",
+				),
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(
+						"circleci_project.test_project",
+						tfjsonpath.New("name"),
+						knownvalue.StringExact("github_project"),
+					),
+					statecheck.ExpectKnownValue(
+						"circleci_project.test_project",
+						tfjsonpath.New("project_provider"),
+						knownvalue.StringExact("github"),
 					),
 					statecheck.ExpectKnownValue(
 						"circleci_project.test_project",
